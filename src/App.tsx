080f2268c8ac5bd3be74d62d860cb88afe62f0df
Boris N'Kuako <nkuako.boris@gmail.com>
2025-09-15 13:40:32 +0200

    chore(first-commit): first commit to setup the repo

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+export function App() {
+  return (
+    <main className="lg:max-w-[500px] ">
+      <h1>Welcome to the world 🌍</h1>
+    </main>
+  );
+}
+
+export default App;
