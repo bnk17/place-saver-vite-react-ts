5ed9cb39953104469f8c99d597822872e0e8d2aa
Boris N'Kuako <nkuako.boris@gmail.com>
2025-10-16 14:20:40 +0200

    refactor(foler-structure): enhance file redability by organizing components, types store by folders

---
+++ client/src/features/Todo/utils.ts
@@ -0,0 +1,8 @@
+import type { ICategoryColorKey } from './types/components.types';
+
+export const categoryColors: Record<ICategoryColorKey, string> = {
+  blue: 'bg-blue-100 text-blue-700',
+  violet: 'bg-violet-200 text-violet-700',
+  amber: 'bg-amber-100 text-amber-600',
+  rose: 'bg-rose-200 text-rose-700',
+};
